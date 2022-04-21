f43553f6d57564223c1b9b5677483622e88aa7d1
Dmitriy Kostyanetskiy (Arcadia) <dmitriy.kostyanetskiy@arcadia.spb.ru>
2022-04-21 21:46:23 +0200

    feat: add host application logic

--- apps/dashboard/src/app/app.component.ts
+++ apps/dashboard/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '@mfe/shared/data-access-user';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'mfe-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  template: `
+    <div class="dashboard-nav">Admin Dashboard</div>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn><router-outlet></router-outlet></ng-template>
+  `,
+  styles: [``],
 })
-export class AppComponent {
-  title = 'dashboard';
+export class AppComponent implements OnInit {
+  public readonly isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(
+    private readonly userService: UserService,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // TODO: unsubscribe on destroy
+    this.isLoggedIn$
+      .pipe(distinctUntilChanged())
+      .subscribe(async (loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigateByUrl('login');
+        } else {
+          this.router.navigateByUrl('');
+        }
+      });
+  }
 }
